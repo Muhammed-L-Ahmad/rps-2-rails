0f38962c7825e21cb8706c578c69c833bc212a22
Muhammed Ahmad <99442871+Muhammed-L-Ahmad@users.noreply.github.com>
2025-09-24 16:54:05 +0000

    added rules.html.erb and aligned code in zebra_controller.rb

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,50 +1,47 @@
 class ZebraController < ApplicationController
 
   def rules
-    # This will look for app/views/zebra/rules.html.erb
     render({ :template => "game_templates/rules" })
   end
 
   def giraffe
     @random_move = ["rock", "paper", "scissors"].sample
-    render({ :template => "game_templates/play_rock"})    
+    
 
-  if @random_move == "rock"
-    @outcome = "tied"
-  elsif @random_move == "paper"
-    @outcome = "lost"
-  else
-    @outcome = "won"
+    if @random_move == "rock"
+      @outcome = "tied"
+    elsif @random_move == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+    render({ :template => "game_templates/play_rock"})    
   end
 
-erb(:play_rock)
 
   def elephant
     @random_move = ["rock", "paper", "scissors"].sample
+
+    if @random_move == "paper"
+      @outcome = "tied"
+    elsif @random_move == "scissors"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
     render({ :template => "game_templates/play_paper"}) 
-
-  if @random_move == "paper"
-    @outcome = "tied"
-  elsif @random_move == "scissors"
-    @outcome = "lost"
-  else
-    @outcome = "won"
-
   end
-
-erb(:play_paper)
-
 
   def lion
     @random_move = ["rock", "paper", "scissors"].sample
+
+    if @random_move == "scissors"
+      @outcome = "tied"
+    elsif @random_move == "rock"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
     render({ :template => "game_templates/play_scissors"}) 
-
-  if @random_move == "scissors"
-    @outcome = "tied"
-  elsif @random_move == "rock"
-    @outcome = "lost"
-  else
-    @outcome = "won"
   end
-
-erb(:play_scissors)
+end
